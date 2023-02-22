2dbe2fe649b3d346a434e62e6bb766fd403d47d1
Min. Silas <olorunishola.silas@gmail.com>
2023-02-22 11:39:09 -0800

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - function that prints 10x alphabets
- *
- * description: print_alphabet_10x
+ * print_alphabet_x10 - function that prints 10x alphabets
  *
  * Return: always 0
  */
-void print_alphabet_10x(void)
 
+void print_alphabet_x10(void)
 {
 	int i;
 	char j;
