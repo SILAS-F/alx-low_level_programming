bec050602e51aa87a296265420fae68fd4e1da00
Min. Silas <olorunishola.silas@gmail.com>
2023-02-24 05:09:46 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - This funtion prints uppercase letters
+ * @c: char to check
+ *
+ * Return 1 or 0
+ **/
+
+int _isupper(int c)
+
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a Read me file for more funtions, and nested loop for my ALX course
